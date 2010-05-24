77e395af2c2eebefc6b2eaaf45710f940ffaffce
robbo <dcchut@gmail.com>
2010-05-24 23:07:28 +1000

    fixed up some small minor insignificant major issues

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (!file_exists('config.php') || !require('config.php'))
 	exit('config.php does not exist');
 
 // we need the API key!
-if (!is_defined('API_KEY'))
+if (!defined('API_KEY'))
 	exit('API_KEY not defined in config.php');
 
 // grab a particular page of recently listened tracks
@@ -19,7 +19,15 @@ function grab_page($user, $page)
 	$url  .= '&api_key=' . urlencode(API_KEY);
 	$url  .= '&limit=200&page=' . (int)$page;
 	
-	return file_get_contents($url);
+	// we use CURL, since PHP is inadequate for web programming
+	$curl  = curl_init();
+	
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_HEADER, 0);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+	
+	return curl_exec($curl);
 }
 	
 // grab this users everything
@@ -34,6 +42,13 @@ function page_runner($user)
 	// we've already grabbed the first page, but who cares, it's only 1 more, right?
 	for ($i = 1; $i <= $total; $i++)
 	{
+		// they have a rubbish webserver, try multiple times if its complete and utter garbage
+		do {
+			$page = grab_page($user, $i);
+		}
+		while (empty($page));
+		
+		// chuck the file away
 		file_put_contents('./temp/' . $user . '_p' . $i . '_' . $total . '.xml', grab_page($user, $i));
 		sleep(1); // they hate spammers!
 	}
@@ -41,4 +56,3 @@ function page_runner($user)
 
 // go you stallion
 page_runner('dcchut');
-
